996b5198c498bdcdaf031cc7c60065325eacb2d7
ariksidney <ariksidney@outlook.com>
2021-06-26 23:33:33 +0200

    Revoke keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCLfXprX2VEpJpNpI5UI5xhFwZNhTMbMpQ",
-    authDomain: "divr-f8ff5.firebaseapp.com",
-    databaseURL: "https://divr-f8ff5.firebaseio.com",
-    projectId: "divr-f8ff5",
-    storageBucket: "divr-f8ff5.appspot.com",
-    messagingSenderId: "581318066998",
-    appId: "1:581318066998:web:fdf7b66796fdac7c",
-    measurementId: "G-3NB9Q2TYKC"
-  
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
   },
-  mapKey: 'AIzaSyC2quI7hVioU-Gn59LWs1K2GDFlzdKUWZ8'
+  mapKey: ''
 };
